13177e711a65d0c978650348465d6fe9f474f4a4
Given <gkibanzait@gmail.com>
2023-06-29 16:39:32 +0200

    added leo's encoder functions

--- ml_logic/encoders.py
+++ ml_logic/encoders.py
@@ -64,14 +64,14 @@ def transform_eng_mfgr(X: pd.DataFrame) -> np.ndarray:
 
     return eng_mfgr_enc
 
-def far_part_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_far_part(X: pd.DataFrame) -> np.ndarray:
     """Transforms far_part using OHE."""
 
     ohe_far_part = OneHotEncoder(sparse_output=False, min_frequency=300).fit(X[['far_part']])
     far_part_encoded = ohe_far_part.transform(X[['far_part']])
     return far_part_encoded
 
-def acft_make_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_acft_make(X: pd.DataFrame) -> np.ndarray:
     """Transforms acft_make using Custom functions and OHE."""
 
     # lists for replacements
@@ -121,21 +121,21 @@ def acft_make_encoder(X: pd.DataFrame) -> np.ndarray:
     acft_make_encoded = ohe_acft_make.transform(X[['acft_make']])
     return acft_make_encoded
 
-def fixed_retractable_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_fixed_retractable(X: pd.DataFrame) -> np.ndarray:
     """Transforms fixed_retractable using OHE."""
 
     ohe_fixed_retractable = OneHotEncoder(sparse_output=False, drop='if_binary').fit(X[['fixed_retractable']])
     fixed_retractable_encoded = ohe_fixed_retractable.transform(X[['fixed_retractable']])
     return fixed_retractable_encoded
 
-def acft_category_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_acft_category(X: pd.DataFrame) -> np.ndarray:
     """Transforms acft_category using OHE."""
 
     ohe_acft_category = OneHotEncoder(sparse_output=False, min_frequency=1000).fit(X[['acft_category']])
     acft_category_encoded = ohe_acft_category.transform(X[['acft_category']])
     return acft_category_encoded
 
-def homebuilt_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_homebuilt(X: pd.DataFrame) -> np.ndarray:
     """Transforms homebuilt using OHE."""
 
     ohe_homebuilt = OneHotEncoder(sparse_output=False, drop='if_binary').fit(X[['homebuilt']])
@@ -143,7 +143,7 @@ def homebuilt_encoder(X: pd.DataFrame) -> np.ndarray:
     return homebuilt_encoded
 
 
-def crew_category_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_crew_category(X: pd.DataFrame) -> np.ndarray:
     """Transforms crew_category using Custom functions and OHE."""
 
     X[['crew_category']] = X[['crew_category']].replace({'KPLT':'PLT', 'CPLT':'PLT'})
@@ -151,16 +151,34 @@ def crew_category_encoder(X: pd.DataFrame) -> np.ndarray:
     crew_category_encoded = ohe_crew_category.transform(X[['crew_category']])
     return crew_category_encoded
 
-def eng_type_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_eng_type(X: pd.DataFrame) -> np.ndarray:
     """Transforms eng_type using OHE."""
 
     ohe_eng_type = OneHotEncoder(sparse_output=False, min_frequency=500).fit(X[['eng_type']])
     eng_type_encoded = ohe_eng_type.transform(X[['eng_type']])
     return eng_type_encoded
 
-def carb_fuel_injection_encoder(X: pd.DataFrame) -> np.ndarray:
+def transform_carb_fuel_injection(X: pd.DataFrame) -> np.ndarray:
     """Transforms carb_fuel_injection using OHE."""
 
     ohe_carb_fuel_injection = OneHotEncoder(sparse_output=False).fit(X[['carb_fuel_injection']])
     carb_fuel_injection_encoded = ohe_carb_fuel_injection.transform(X[['carb_fuel_injection']])
+
     return carb_fuel_injection_encoded
+
+def transform_dprt_dest_apt_id(X: pd.DataFrame) -> np.ndarray:
+    """Transforms certs_held using Custom functions."""
+
+    X.loc[X['dest_apt_id'] == 'NONE', 'dest_apt_id'], X.loc[X['dest_apt_id'] == 'PVT', 'dest_apt_id'] = 0, 0 # None and PVT -> 0
+    X.loc[X['dest_apt_id'] !=0, 'dest_apt_id'] = 1 # values != 0 -> 1
+
+    return X.to_numpy()
+
+
+def transform_pc_professional(X: pd.DataFrame) -> np.ndarray:
+    """Transforms pc_professional using Custom functions."""
+
+    X['pc_profession'].replace('UNK', 'No', inplace=True)
+    X['pc_profession'].replace(['Yes', 'No'], [1, 0], inplace=True)
+
+    return X.to_numpy()
