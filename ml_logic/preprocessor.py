423ea5abb80ca6ab9f43f2e6c97168a9cb2a86e3
Given <gkibanzait@gmail.com>
2023-06-30 09:11:09 +0200

    reverted to aaron's last commit before leaving work - 2a7a2225491e9d41e0f5a02dd013aaa742e1db74

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -4,19 +4,49 @@ import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
-from encoders import transform_far_part
+from encoders import transform_yes_no, transform_gender, transform_type_insp, transform_far_part
 
 
 def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
     """Preprocesses the features of the cleaned dataset."""
 
-    # far_part
-    X = pd.merge(X, transform_far_part(X), left_index=True, right_index=True)
-    X.drop(columns=['far_part'], inplace=True)
+    # transform_yes_no
+    transform_yes_no(X)
 
-    # next encoder
-    X = X
-    X.drop()
+    # transform_gender
+    transform_gender(X)
+
+    # transform_type_insp
+    X = transform_type_insp(X)
+
+    # transform_type_fly
+
+    # transform_eng_mfgr
+
+    # transform_far_part
+    X = transform_far_part(X)
+
+    # transform_acft_make
+
+    # transform_fixed_retractable
+
+    # transform_acft_category
+
+    # transform_homebuilt
+
+    # transform_crew_category
+
+    # transform_eng_type
+
+    # transform_carb_fuel_injection
+
+    # transform_dprt_dest_apt_id
+
+    # transform_flt_plan_filed **
+
+    # transform_pc_professional
+
 
     X_processed = X.copy()
+
     return X_processed
---
+++ params.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+DATA_SIZE = os.environ.get("DATA_SIZE")
+CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE"))
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_REGION = os.environ.get("GCP_REGION")
+BQ_DATASET = os.environ.get("BQ_DATASET")
+BQ_TABLE = os.environ.get("BQ_TABLE")
+BQ_REGION = os.environ.get("BQ_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+INSTANCE = os.environ.get("INSTANCE")
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
+MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
+MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
+PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
+PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
+
+# VARIABLES NOT SET UP YET
+# GCR_IMAGE = os.environ.get("GCR_IMAGE")
+# GCR_REGION = os.environ.get("GCR_REGION")
+# GCR_MEMORY = os.environ.get("GCR_MEMORY")
+
+##################  CONSTANTS  #####################
+LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), "code", "AaronDV17", "wingman-project", ".data_cache")
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), "code", "AaronDV17", "wingman-project", ".training_outputs")
