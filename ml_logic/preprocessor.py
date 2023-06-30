ec3a3bc6f235763e612e19aebbc929c195cb8597
Given <gkibanzait@gmail.com>
2023-06-30 10:43:12 +0200

    started with implementing and concatinating encoder outputs

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -4,31 +4,41 @@ import pandas as pd
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
-from encoders import transform_yes_no, transform_gender, transform_type_insp, transform_far_part
+from encoders import *
 
 
 def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
     """Preprocesses the features of the cleaned dataset."""
 
     # transform_yes_no -G
+    certs_held = transform_yes_no(X[['certs_held']])
+    second_pilot = transform_yes_no(X[['second_pilot']])
+    site_seeing = transform_yes_no(X[['site_seeing']])
+    air_medical = transform_yes_no(X[['air_medical']])
+
 
     # transform_gender -G
+    crew_sex = transform_gender(X[['crew_sex']])
 
     # transform_type_insp -A
 
     # transform_type_fly -G
+    type_fly = transform_type_fly(X[['type_fly']])
 
     # transform_eng_mfgr -A
 
     # transform_far_part -A
 
     # transform_acft_make -G
+    acft_make = transform_acft_make(X[['acft_make']])
 
     # transform_fixed_retractable -G
+    fixed_retractable = transform_fixed_retractable(X[['fixed_retractable']])
 
     # transform_acft_category -A
 
     # transform_homebuilt -G
+    homebuilt = transform_yes_no(X[['homebuilt']])
 
     # transform_crew_category -A
 
@@ -42,6 +52,10 @@ def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
 
     # transform_pc_professional -L
 
-    X_processed = X.copy()
+
+
+    X_processed = pd.concat([
+        certs_held, second_pilot, site_seeing, air_medical, crew_sex, type_fly, acft_make, fixed_retractable, homebuilt
+    ], axis=1)
 
     return X_processed
