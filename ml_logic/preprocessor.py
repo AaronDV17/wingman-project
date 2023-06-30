3e94f6a191f0b8c3cab9ebe56cecfbaf9e743cad
Aaron de Vroede <aaron.devroede0@gmail.com>
2023-06-30 10:54:45 +0200

    Reassigned task between G & A in preprocessor.py.

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -10,24 +10,21 @@ from encoders import *
 def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
     """Preprocesses the features of the cleaned dataset."""
 
-    # transform_yes_no -G
     certs_held = transform_yes_no(X[['certs_held']])
     second_pilot = transform_yes_no(X[['second_pilot']])
     site_seeing = transform_yes_no(X[['site_seeing']])
     air_medical = transform_yes_no(X[['air_medical']])
 
 
-    # transform_gender -G
     crew_sex = transform_gender(X[['crew_sex']])
 
-    # transform_type_insp -A
+    # transform_type_insp -G
 
-    # transform_type_fly -G
     type_fly = transform_type_fly(X[['type_fly']])
 
-    # transform_eng_mfgr -A
+    # transform_eng_mfgr -G
 
-    # transform_far_part -A
+    X = transform_far_part(X[['far_part']])
 
     # transform_acft_make -G
     acft_make = transform_acft_make(X[['acft_make']])
@@ -35,9 +32,8 @@ def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
     # transform_fixed_retractable -G
     fixed_retractable = transform_fixed_retractable(X[['fixed_retractable']])
 
-    # transform_acft_category -A
+    # transform_acft_category -G
 
-    # transform_homebuilt -G
     homebuilt = transform_yes_no(X[['homebuilt']])
 
     # transform_crew_category -A
@@ -55,7 +51,15 @@ def preprocess_features(X: pd.DataFrame) -> pd.DataFrame:
 
 
     X_processed = pd.concat([
-        certs_held, second_pilot, site_seeing, air_medical, crew_sex, type_fly, acft_make, fixed_retractable, homebuilt
+        certs_held,
+        second_pilot,
+        site_seeing,
+        air_medical,
+        crew_sex,
+        type_fly,
+        acft_make,
+        fixed_retractable,
+        homebuilt
     ], axis=1)
 
     return X_processed
