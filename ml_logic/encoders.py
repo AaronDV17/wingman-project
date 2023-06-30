61093f6feaad184ff86f4453623c02a5fec2a30d
Given <gkibanzait@gmail.com>
2023-06-30 09:18:10 +0200

    reverting to old code

--- ml_logic/encoders.py
+++ ml_logic/encoders.py
@@ -3,30 +3,28 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 
 
-def transform_yes_no(X: pd.DataFrame) -> pd.DataFrame:
-    """Transforms the 'Y' and 'N' values to 1 and 0 respectively."""
-    X = X.replace({'Y': 1, 'N': 0}, inplace=True)
-    return X
+def transform_yes_no(X: pd.DataFrame) -> np.ndarray:
+    """Transforms the 'yes' and 'no' values to 1 and 0 respectively."""
 
-def transform_gender(X: pd.DataFrame) -> pd.DataFrame:
+    yn_categories = ["N", "Y"]
+    bin_encoder_1 = OrdinalEncoder(categories=[yn_categories])
+
+    return bin_encoder_1.fit_transform(X)
+
+def transform_gender(X: pd.DataFrame) -> np.ndarray:
     """Transforms 'M" and 'F' values to 1 and 0 respectively."""
-    X = X.replace({'M': 1, 'F': 0}, inplace=True)
-    return X
+
+    mf_categories = ["M", "F"]
+    bin_encoder_2 = OrdinalEncoder(categories=[mf_categories])
+
+    return bin_encoder_2.fit_transform(X)
 
 def transform_type_insp(X: pd.DataFrame) -> np.ndarray:
     """Transforms Inspection types:ANNL, 100H, COND, UNK, COAW, AAIP  using OHE."""
 
-    # Replace 'UNK' with 'ANNL' in the 'type_last_insp' column
-    X.replace({'type_last_insp': {'UNK': 'ANNL'}}, inplace=True)
+    ohe = OneHotEncoder(sparse_output=False, drop='if_binary')
 
-    ohe = OneHotEncoder(sparse=False, drop='if_binary').fit(X[['type_last_insp']])
-    type_insp_encoded = ohe.transform(X[['type_last_insp']])
-
-    type_insp_encoded_df = pd.DataFrame(type_insp_encoded, columns=ohe.get_feature_names_out())
-
-    X[ohe.get_feature_names_out()] = ohe.transform(X[['type_last_insp']])
-    X.drop(columns=['type_last_insp'], inplace=True)
-    return X
+    return ohe.fit_transform(X)
 
 def transform_type_fly(X: pd.DataFrame) -> np.ndarray:
     """Transforms type_fly using Custom function."""
@@ -66,18 +64,12 @@ def transform_eng_mfgr(X: pd.DataFrame) -> np.ndarray:
 
     return eng_mfgr_enc
 
-def transform_far_part(X: pd.DataFrame) -> pd.DataFrame:
+def transform_far_part(X: pd.DataFrame) -> np.ndarray:
     """Transforms far_part using OHE."""
 
     ohe_far_part = OneHotEncoder(sparse_output=False, min_frequency=300).fit(X[['far_part']])
     far_part_encoded = ohe_far_part.transform(X[['far_part']])
-
-    far_part_encoded_df = pd.DataFrame(far_part_encoded, columns=ohe_far_part.get_feature_names_out())
-
-    X = pd.merge(X, far_part_encoded_df, left_index=True, right_index=True)
-    X.drop(columns=['far_part'], inplace=True)
-
-    return X
+    return far_part_encoded
 
 def transform_acft_make(X: pd.DataFrame) -> np.ndarray:
     """Transforms acft_make using Custom functions and OHE."""
@@ -182,10 +174,6 @@ def transform_dprt_dest_apt_id(X: pd.DataFrame) -> np.ndarray:
 
     return X.to_numpy()
 
-def transform_flt_plan_filed(X: pd.DataFrame) -> pd.DataFrame:
-    """Transforms transform_flt_plan_filed using Custom functions."""
-
-    return X
 
 def transform_pc_professional(X: pd.DataFrame) -> np.ndarray:
     """Transforms pc_professional using Custom functions."""
